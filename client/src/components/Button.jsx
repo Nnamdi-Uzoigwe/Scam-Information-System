900ff40c4b09d2b0f73f5f5409c03768282f7054
nnamdi <uzonnamdi31@gmail.com>
2025-04-23 21:57:15 +0100

    ...

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -1,6 +1,6 @@
 export default function Button({ children }) {
     return (
-        <div className="cursor-pointer bg-[#0F766E] w-fit py-2 px-4 md:px-6 text-white flex items-center font-medium rounded-lg transition duration-200 transform hover:scale-105">
+        <div className="cursor-pointer bg-[#0F766E] w-fit py-2 px-[10px] md:px-6 text-white flex items-center font-medium rounded-lg transition duration-200 transform hover:scale-105">
             {children}
         </div>
     )
