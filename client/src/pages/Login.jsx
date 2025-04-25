1107276b0fdc08604c20695222c30b1ac745b34e
nnamdi <uzonnamdi31@gmail.com>
2025-04-25 13:36:06 +0100

    new changes effected

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -31,7 +31,7 @@ export default function Login() {
     }
   
     try {
-      const res = await fetch('https://scam-information-system-1.onrender.com/api/auth/register', {
+      const res = await fetch('https://scam-information-system-1.onrender.com/api/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
