b5f5039f787a3e1cdf808f294acb044ee14ef65d
nnamdi <uzonnamdi31@gmail.com>
2025-05-05 18:29:18 +0100

    monday's commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,31 +13,40 @@ import Contact from "./pages/Contact";
 import ScamDetail from "./pages/ScamDetail";
 import RequireAuth from "./components/RequireAuth";
 import ScrollToTop from "./components/ScrollToTop";
+import SearchDatabase from "./pages/SearchDatabase";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   const location = useLocation();
 
   
-  const noFooterRoutes = ['/dashboard', '/login', '/register'];
+  const noFooterRoutes = ['/dashboard', '/login', '/register', '/report-scam'];
 
   return (
     <div className="min-h-screen flex flex-col">
       
-      {location.pathname !== '/dashboard' && location.pathname !== '/login' && location.pathname !== '/register' && <Navbar />}
+      {location.pathname !== '/dashboard' 
+        && location.pathname !== '/login' 
+        && location.pathname !== '/register' 
+        && location.pathname !== '/report-scam' &&
+        <Navbar />
+      }
 
       <div className="flex-grow">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/*" element={<PageNotFound />} /> 
-          <Route element={<RequireAuth />}>
-          <Route path="/report-scam" element={<ReportScam />} />
+          <Route element={<ProtectedRoute />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/report-scam" element={<ReportScam />} />
           </Route>
+          <Route path="/search" element={<SearchDatabase />} />
           <Route path="/contact" element={<Contact/>} />
           <Route path="/scam/:id" element={<ScamDetail />} />
+
+          <Route path="/*" element={<PageNotFound />} /> 
         </Routes>
       </div>
 
